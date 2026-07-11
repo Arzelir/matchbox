e49351b14dc2c1a72cb4cfcb636320f4682d40f8
Arzelir <85701665+Arzelir@users.noreply.github.com>
2026-07-11 13:03:20 -0400

    goodness gracious me

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,9 +1,5 @@
 export async function getUsers() {
     const response = await fetch("/api/users");
 
-    const text = await response.text();
-
-    console.log(text);
-
-    return text;
+    return response.json();
 }
