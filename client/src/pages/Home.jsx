3327440bdf482fe4adb45e81a964676ade9249b6
Arzelir <85701665+Arzelir@users.noreply.github.com>
2026-07-11 12:08:49 -0400

    testing home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,26 @@
+import { getUsers } from "../services/api";
+
+
 function Home() {
-  return (
-    <h1>Home Page Test</h1>
+
+        const [users, setUsers] = useState([]);
+    
+        useEffect(() => {
+            getUsers()
+                .then(setUsers);
+        }, []);
+
+    return (
+      
+      <div>
+            <h1>Home Page Test</h1>
+            <h1>Dashboard</h1>
+            <ul>
+                {users.map(user => (
+                    <li key={user.id}>{user.name}</li>
+                ))}
+            </ul>
+        </div>
   );
 }
 
