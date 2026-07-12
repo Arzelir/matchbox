9d5e82546d2874ccde9ec93a2326eebe36c77703
Arzelir <85701665+Arzelir@users.noreply.github.com>
2026-07-12 01:17:25 -0400

    narrowing error

--- client/src/components/EventCard.jsx
+++ client/src/components/EventCard.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 
 export default function EventCard({ event }) {
-	console.log(event);
+	console.log("EventCard ", event);
 	return (
 		<div className="event-card">
 			<h3>{event.name}</h3>
