e4f945aee9d0f616cdbee53b2465278f4ad728cf
Arzelir <85701665+Arzelir@users.noreply.github.com>
2026-07-12 00:27:31 -0400

    fixed event card name

--- client/src/components/EventCard.jsx
+++ client/src/components/EventCard.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default function EventCard({ event }) {
 	return (
 		<div className="event-card">
-			<h3>{event.title}</h3>
+			<h3>{event.name}</h3>
 
 			<p>
 				<strong>Date:</strong> {event.date}
