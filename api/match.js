1efcb7c7694b1d7d296c5423afa61051cfe09996
Arzelir <85701665+Arzelir@users.noreply.github.com>
2026-07-11 20:44:41 -0400

    fixed match making

--- api/match.js
+++ api/match.js
@@ -1,7 +1,34 @@
 import db from "./db.js";
 
 export default async function handler(req, res) {
-    const result = await db.execute("SELECT User.user_id, User.name, User.picture_path, User.description FROM User JOIN Registration ON User.user_id = Registration.user_id WHERE Registration.event_id = ? AND Registration.has_team = 0;");
 
-    res.status(200).json(result.rows);
+    const { eventId } = req.query;
+
+    try {
+
+        const result = await db.execute({
+            sql: `
+                SELECT 
+                    User.user_id,
+                    User.name,
+                    User.picture_path,
+                    User.description
+                FROM User
+                JOIN Registration
+                    ON User.user_id = Registration.user_id
+                WHERE Registration.event_id = ?
+                AND Registration.has_team = 0
+            `,
+            args: [eventId]
+        });
+
+        res.status(200).json(result.rows);
+
+    } catch(error) {
+
+        res.status(500).json({
+            error: error.message
+        });
+
+    }
 }
